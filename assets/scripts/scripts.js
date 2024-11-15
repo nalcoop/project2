8febb63f5a3ecca46e170c842dd1fb3a5cacfd1e
nalcoop <nic4340@rit.edu>
2024-11-15 01:57:19 -0500

    testing

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -47,74 +47,95 @@ window.addEventListener("resize", function () {
 
 
 
-let currentSlide = 0;
-
-function changePhoto(index) {
-  const slides = document.querySelectorAll(".carousel-img img");
-
-  if (slides.length < 0) {
-    return false;
-  }
-  if (index >= slides.length) {
-    currentSlide = 0;
-  } else if (index < 0) {
-    currentSlide = slides.length - 1;
-  } else {
-    currentSlide = index;
-  }
-
-  slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === currentSlide) {
-      slide.classList.add("active");
-    }
-  });
-}
-
-function changeSlide(direction) {
-  changePhoto(currentSlide + direction);
-}
-
-changePhoto(currentSlide);
-
-var newImage = new Array();
-var captionArray = [document.getElementById('walters'), document.getElementById('bma'), document.getElementById('blackInWaxs'), document.getElementById('rfl')];
-const museumMap = {
-  'assets/img/museum1.jpg': "walters",
-  'assets/img/museum2.jpeg': "bma",
-  'assets/img/museum3.jpeg': "blacksInWaxs",
-  'assets/img/museum4.jpeg': "rfl"
+let currentSlides ={
+    "carousel-aquarium":0,
+    "carousel-science" :0
 };
-function init() {
-  const images = document.querySelectorAll('.museums')
-  images.forEach(img => {
-    img.addEventListener("click", function () {
-      changeMe(this);
-    })
+
+function changeSlide(step, carouselId) {
+  const carousel =document.getElementById(carouselId);
+  const slides = carousel.querySelectorAll(".carousel-img");
+  const totalSlides= slides.length;
+  currentSlides[carouselId] += step;
+
+  if(currentSlides[carouselId] >= totalSlides){
+    currentSlides[carouselId]=0;
+  } else if(currentSlides[carouselId] < 0){
+    currentSlides[carouselId] -1;
+  } 
+
+  slides.forEach((slide,i)=>{
+    slide.classList.remove("active");
+    if(i=== currentSlides[carouselId])
+        slide.classList.add("active");
+  
   });
 
-  captionArray.forEach(caption => {
-    caption.style.display="none";
-  });
 }
 
+// function changePhoto(index) {
+//   const slides = document.querySelectorAll(".carousel-img img");
+
+//   if (slides.length < 0) {
+//     return false;
+//   }
+//   if (index >= slides.length) {
+//     currentSlide = 0;
+//   } else if (index < 0) {
+//     currentSlide = slides.length - 1;
+//   } else {
+//     currentSlide = index;
+//   }
+
+//   slides.forEach((slide, i) => {
+//     slide.classList.remove("active");
+//     if (i === currentSlide) {
+//       slide.classList.add("active");
+//     }
+//   });
+// }
 
 
-function changeMe(dom) {
-  const imageSource = dom.src;
-  const target = museumMap[imageSource];
-  if(!target){return false};
+// changePhoto(currentSlide);
 
-  captionArray.forEach(caption => {
-    caption.style.display="none";
-  });
+// var newImage = new Array();
+// var captionArray = [document.getElementById('walters'), document.getElementById('bma'), document.getElementById('blackInWaxs'), document.getElementById('rfl')];
+// const museumMap = {
+//   'assets/img/museum1.jpg': "walters",
+//   'assets/img/museum2.jpeg': "bma",
+//   'assets/img/museum3.jpeg': "blacksInWaxs",
+//   'assets/img/museum4.jpeg': "rfl"
+// };
+// function init() {
+//   const images = document.querySelectorAll('.museums')
+//   images.forEach(img => {
+//     img.addEventListener("click", function () {
+//       changeMe(this);
+//     })
+//   });
+
+//   captionArray.forEach(caption => {
+//     caption.style.display="none";
+//   });
+// }
+
+
+
+// function changeMe(dom) {
+//   const imageSource = dom.src;
+//   const target = museumMap[imageSource];
+//   if(!target){return false};
+
+//   captionArray.forEach(caption => {
+//     caption.style.display="none";
+//   });
 
   
-  const targetCaption = document.getElementById(target);
-  if(targetCaption){
-    targetCaption.style.display="block";
-  }
-}
+//   const targetCaption = document.getElementById(target);
+//   if(targetCaption){
+//     targetCaption.style.display="block";
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded",init);
+// document.addEventListener("DOMContentLoaded",init);
 
