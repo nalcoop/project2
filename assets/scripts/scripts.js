2e949e6e06307c53a6286abb05a00ce9339d35b1
nalcoop <nic4340@rit.edu>
2024-11-15 02:03:34 -0500

    argh

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -73,69 +73,45 @@ function changeSlide(step, carouselId) {
 
 }
 
-// function changePhoto(index) {
-//   const slides = document.querySelectorAll(".carousel-img img");
 
-//   if (slides.length < 0) {
-//     return false;
-//   }
-//   if (index >= slides.length) {
-//     currentSlide = 0;
-//   } else if (index < 0) {
-//     currentSlide = slides.length - 1;
-//   } else {
-//     currentSlide = index;
-//   }
+var newImage = new Array();
+var captionArray = [document.getElementById('walters'), document.getElementById('bma'), document.getElementById('blackInWaxs'), document.getElementById('rfl')];
+const museumMap = {
+  'assets/img/museum1.jpg': "walters",
+  'assets/img/museum2.jpeg': "bma",
+  'assets/img/museum3.jpeg': "blacksInWaxs",
+  'assets/img/museum4.jpeg': "rfl"
+};
+function init() {
+  const images = document.querySelectorAll('.museums')
+  images.forEach(img => {
+    img.addEventListener("click", function () {
+      changeMe(this);
+    })
+  });
 
-//   slides.forEach((slide, i) => {
-//     slide.classList.remove("active");
-//     if (i === currentSlide) {
-//       slide.classList.add("active");
-//     }
-//   });
-// }
-
-
-// changePhoto(currentSlide);
-
-// var newImage = new Array();
-// var captionArray = [document.getElementById('walters'), document.getElementById('bma'), document.getElementById('blackInWaxs'), document.getElementById('rfl')];
-// const museumMap = {
-//   'assets/img/museum1.jpg': "walters",
-//   'assets/img/museum2.jpeg': "bma",
-//   'assets/img/museum3.jpeg': "blacksInWaxs",
-//   'assets/img/museum4.jpeg': "rfl"
-// };
-// function init() {
-//   const images = document.querySelectorAll('.museums')
-//   images.forEach(img => {
-//     img.addEventListener("click", function () {
-//       changeMe(this);
-//     })
-//   });
-
-//   captionArray.forEach(caption => {
-//     caption.style.display="none";
-//   });
-// }
+  captionArray.forEach(caption => {
+    caption.style.display="none";
+  });
+}
 
 
 
-// function changeMe(dom) {
-//   const imageSource = dom.src;
-//   const target = museumMap[imageSource];
-//   if(!target){return false};
+function changeMe(dom) {
+  const imageSource = dom.src;
+  const target = museumMap[imageSource];
+  if(!target){return false};
 
-//   captionArray.forEach(caption => {
-//     caption.style.display="none";
-//   });
+  captionArray.forEach(caption => {
+    caption.style.display="none";
+  });
 
   
-//   const targetCaption = document.getElementById(target);
-//   if(targetCaption){
-//     targetCaption.style.display="block";
-//   }
-// }
+  const targetCaption = document.getElementById(target);
+  if(targetCaption){
+    targetCaption.style.display="block";
+  }
+}
 
-// document.addEventListener("DOMContentLoaded",init);
+document.addEventListener("DOMContentLoaded",init);
 
