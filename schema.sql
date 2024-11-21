09d97fbbd872159e4a2f4bf0fc4e64751418b140
nalcoop <nic4340.g.rit.edu>
2024-11-21 18:16:16 -0500

    maybe this will work

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
+DROP TABLE IF EXISTS `Survey`;
 CREATE TABLE `Survey` (
   `Id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `Name` text(255) COLLATE 'utf8_general_ci' NOT NULL,
   `Email` text(255) COLLATE 'utf8_general_ci' NOT NULL,
-  `Message` longtext COLLATE 'utf8_general_ci' NOT NULL,
+  `Message` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Datetime` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE='InnoDB' COLLATE 'utf8_general_ci';
 
